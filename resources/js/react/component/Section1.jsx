211001bf4e720fff28ff45d0583b2f4b95346d2b
User Namemaranabouassaf <abouassafmaram@gmail.com>
2023-03-11 05:46:22 +0200

    test

---
+++ resources/js/react/component/Section1.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+export default ()=>{
+    return(
+        <>
+          <Row className="mx-0">
+            <Col md="4">
+                i love
+            </Col>
+          </Row>
+            </>
+    )
+}
---
+++ resources/js/react/index.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import {createRoot} from "react-dom/client"
+import Section1 from "./component/Section1";
+function App() {
+return(
+    <>
+    helllllooooooo
+        </>
+)
+}
+createRoot(document.getElementById("root")).render(<App/>)
